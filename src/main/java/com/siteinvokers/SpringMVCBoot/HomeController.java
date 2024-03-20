aae624181669324c937996aca459bef7f801364e
Shubham-Haldkar <shubham.haldkar@gmail.com>
2024-03-20 22:03:30 +0530

    Using ModelAndView method

--- src/main/java/com/siteinvokers/SpringMVCBoot/HomeController.java
+++ src/main/java/com/siteinvokers/SpringMVCBoot/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -18,19 +19,25 @@ public class HomeController {
     public String home(){
 		return "index.jsp" ;
     }
+
+    // @RequestMapping("add")
+    // public String add( @RequestParam("num1") int i, @RequestParam("num2") int j,
+    //                 HttpSession session ){
+    //     // int i = Integer.parseInt(req.getParameter("num1"));
+    //     // int j = Integer.parseInt(req.getParameter("num2"));
+    //     int sum = i+j ;
+    //     // passing value by session
+    //     // HttpSession session =  req.getSession();
+    //     session.setAttribute("sum", sum) ;
+    //     return "result.jsp" ;
+    // }
+
     @RequestMapping("add")
-    public String add( @RequestParam("num1") int i, @RequestParam("num2") int j,
-                    HttpSession session ){
-        // int i = Integer.parseInt(req.getParameter("num1"));
-        // int j = Integer.parseInt(req.getParameter("num2"));
-
-        int sum = i+j ;
-   
-        // passing value by session
-        // HttpSession session =  req.getSession();
-        session.setAttribute("sum", sum) ;
-
-        return "result.jsp" ;
+    public ModelAndView add( @RequestParam("num1") int i, @RequestParam("num2") int j  ){
+      ModelAndView mv = new ModelAndView() ;
+      mv.setViewName("result.jsp");
+      int sum = i+j ;
+      mv.addObject("sum", sum) ;
+      return  mv ;
     }
-
 }
